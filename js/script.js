2b18442138ece0c1a86e92d65a32264559c8ad24
JustCodeAadarsh <155641365+JustCodeAadarsh@users.noreply.github.com>
2024-01-04 18:27:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ document.getElementById("myButton").addEventListener("click", function() {
 });
 
 document.getElementById("myButton2").addEventListener("click", function() {
-    window.location.href = "https://www.example.com";
+    window.location.href = "https://youtu.be/0tOXxuLcaog?si=iatSu01qwK8DhkkC";
 });
 
 document.getElementById("myFa").addEventListener("click", function() {
     window.location.href = "https://www.example.com";
+});
+
+document.getElementById("myButton3").addEventListener("click", function() {
+    window.location.href = "https://github.com/JustCodeAadarsh";
 });
