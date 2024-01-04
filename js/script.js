8760b4026ea760edd8c21dcac65d92c296f7dc08
JustCodeAadarsh <155641365+JustCodeAadarsh@users.noreply.github.com>
2024-01-04 18:17:36 +0530

    Add files via upload
    YES

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "skills.html"
+});
+
+document.getElementById("myButton2").addEventListener("click", function() {
+    window.location.href = "https://www.example.com";
+});
+
+document.getElementById("myFa").addEventListener("click", function() {
+    window.location.href = "https://www.example.com";
+});
